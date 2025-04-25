497a73f8060c0c1f0bfa6890f016e12b78aff82a
fern <frpm450@gmail.com>
2025-04-24 23:26:48 -0300

    test

---
+++ src/main/java/net/fern/tutorialmod/TutorialmodClient.java
@@ -0,0 +1,10 @@
+package net.fern.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialmodClient implements ClientModInitializer  {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
